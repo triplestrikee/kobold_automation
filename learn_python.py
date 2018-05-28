d9ce8c3ec15244b529de7da6356dfa13935ccbda
Han <shenhanaholic@gmail.com>
2018-05-28 14:03:58 -0700

    add current time stamp as folder name

--- learn_python.py
+++ learn_python.py
@@ -1,3 +1,8 @@
+# import the follwoing modulars
+import datetime
+import sys
+import os
+
 str = input('Please enter a string: ')
 count = 0
 for i in range(len(str)):
@@ -7,3 +12,7 @@ for i in range(len(str)):
 print('Number of the whitespace in the string is: ',count)
 
 print('Hi ni hao {}'.format(count))
+
+now = datetime.datetime.now()
+newDirName = "Kobold_XB1_Test " + now.strftime("%Y_%m_%d-%H%M")
+print(newDirName)
